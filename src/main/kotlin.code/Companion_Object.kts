750d3639714d02af0ea4c0f1f34bc2b1961058d9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-09 10:26:48 -0300

    Companion Object add

---
+++ src/main/kotlin.code/Companion_Object.kts
@@ -0,0 +1,14 @@
+/**************************************
+You can create companion objects using
+the keyword companion object
+**********************************/
+
+class lesson {
+    companion object objectStudy {
+        var name: String = ""
+
+        fun getString(): String {
+            return "this is a object"
+        }
+    }
+}
