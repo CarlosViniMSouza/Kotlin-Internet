6a2e67813f7be9ec10c37923f5c1899b6913554f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-16 17:28:57 -0300

    break-loops addexit

---
+++ src/main/kotlin.code/Break_Loops.kts
@@ -0,0 +1,20 @@
+for (i in 1..7){
+    println(i)
+    if (i == 3){
+        println("i is equal 3")
+        /*here doing the break loop
+        enclosing loop in sequence
+         */
+    }
+}
+
+/*
+Output:
+1
+2
+i is equal 3
+4
+5
+6
+7
+ */
