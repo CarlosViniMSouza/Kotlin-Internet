35516cc4bbd1a2488456c00f33c98eb866735476
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-04 10:47:17 -0300

    lambda expression add

---
+++ src/main/kotlin.code/Lambda_Exp.kt
@@ -0,0 +1,10 @@
+/*
+lamba expression or simply 'lambda' is as anonymous function; a function without name specific.
+These functions are passed immediately as an expression without declaration
+ */
+
+fun main(){
+    val hello = { println("Salve Salve Galerinha!") }
+
+    hello()
+}
